49b750b70c7b6d9a99262c07989d51b30fcca041
SofFM <33416390+SofFM@users.noreply.github.com>
2017-12-18 17:38:46 +0300

    Create window.hpp

---
+++ taskThree-window/include/taskThree/window.hpp
@@ -0,0 +1,79 @@
+#ifndef WND_HPP
+#define WND_HPP
+
+
+#include<cstddef>
+#include<ostream>
+
+namespace wn{
+
+class Window;
+
+class Common{
+    friend class Window;
+    friend std::ostream& operator << (std::ostream& os, const Window& window);
+
+    std::size_t step;
+    std::size_t owners;
+    bool* data;
+
+    Common();
+
+    Common(std::size_t h_, std::size_t w_);
+
+    Common(Window& window, std::size_t x, std::size_t y, std::size_t w);
+
+
+    Common (const Common& info);
+    Common (Common&& info);
+
+    Common& operator = (const Common& info);
+    Common& operator = (Common&& info);
+
+
+    ~Common();
+};
+
+class Window{
+        friend class Common;
+
+        std::size_t h;
+        std::size_t w;
+        Common info;
+        bool* start;
+
+        void dec_owners();
+        void inc_owners();
+
+    public:
+        Window();
+        Window(std::size_t h_, std::size_t w_);
+
+        Window (const Window& window);
+        Window (Window&& window);
+
+        Window (Window& window, std::size_t x, std::size_t y, std::size_t h_, std::size_t w_);
+
+
+        ~Window();
+
+        friend std::ostream& operator << (std::ostream& os, const Window& window);
+
+        Window& operator = (const Window& window);
+
+        Window& operator = (Window&& window);
+
+        bool operator == (const Window& window) const;
+
+        bool operator () (std::size_t x, std::size_t y) const;
+
+        bool& operator () (std::size_t x, std::size_t y);
+
+        explicit operator bool() const;
+
+        std::size_t get_width() const;
+        std::size_t get_height() const;
+        std::size_t check_owners() const;
+};
+}
+#endif // WND_HPP
